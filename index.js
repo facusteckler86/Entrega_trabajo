62db61911f139537e5d254cd4980fef4ca3d66b5
facusteckler86 <facundomsteckler@gail.com>
2023-03-04 17:34:15 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// let comida1 = pan;
+// let comida 2 = facturas;
+// let comida3 = sanguches de miga;
+
+
+
+const productos = {producto1 = "Pan de Campo", producto2 = "Medialunas", producto3 = "sanguches de miga"};
+console.log(productos.producto1);
