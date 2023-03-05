9b5b3d74d539bdf963b92ba65e580ad80ae4ecb5
facusteckler86 <facundomsteckler@gail.com>
2023-03-05 02:03:22 -0300

    tercer commit

--- index.js
+++ index.js
@@ -4,5 +4,20 @@
 
 
 
-const productos = {producto1 = "Pan de Campo", producto2 = "Medialunas", producto3 = "sanguches de miga"};
-console.log(productos.producto1);
+// const productos = {producto1 = "Pan de Campo", producto2 = "Medialunas", producto3 = "sanguches de miga"};
+// console.log(productos.producto1);
+
+
+
+function productos (pan de campo, precio, medialunas, sanguches){
+
+this.pan de campo = pan de campo;
+this.medialunas = medialunas;
+this.sanguches = sanguches;
+this.precio= precio;
+}
+prompt (" Que desea comprar?");
+const productos1 = new productos (pan de campo, $ 300);
+console.log("productos1");
+
+
