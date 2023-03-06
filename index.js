ac1a7da663036e8e85aad89745863146d7bbe462
facusteckler86 <facundomsteckler@gail.com>
2023-03-06 00:59:12 -0300

    cuarto commit

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-// let comida1 = pan;
-// let comida 2 = facturas;
-// let comida3 = sanguches de miga;
+// modelo de carrito de compra para panaderia
 
+class producto{
 
-
-// const productos = {producto1 = "Pan de Campo", producto2 = "Medialunas", producto3 = "sanguches de miga"};
-// console.log(productos.producto1);
-
-
-
-function productos (pan de campo, precio, medialunas, sanguches){
+constructor(pan de campo, medialunas, sanguches, precio, imagen){
 
 this.pan de campo = pan de campo;
 this.medialunas = medialunas;
 this.sanguches = sanguches;
-this.precio= precio;
+this.precio = precio;
+this.imagen = imagen;
+this.vendido = false;
+
 }
-prompt (" Que desea comprar?");
-const productos1 = new productos (pan de campo, $ 300);
+let productos = prompt("Ingrese que producto desea");
+
+
+const productos1 = new productos (pan de campo: "pan de campo", precio : 300, imagen: imagen);
 console.log("productos1");
 
+const productos2 = new productos (medialunas: "medialunas", precio : 600 , imagen: imagen);
+console.log("productos2");
 
+const productos3 = new productos (sanguches: "sanguches", precio: 250, imagen: "imagen");
+console.log("productos3");
+
+
+}
+
+}
+
+const productos ["pan de campo", "medialunas", "sanguches"];
+
+for (let index = 0; index < productos.length; index++) {
+    const element = productos[index];
+    
+}
