31a431f95150ccca144e8fccaf10c6f187e49172
facusteckler86 <facundomsteckler@gail.com>
2023-03-12 16:50:47 -0300

    primer arreglo

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 // modelo de carrito de compra para panaderia
 
-class productos{
+//saludo inicial.
+
+panaderia = cargaClientes ();
+
+let mensaje = "Bienvenid@: ";
+
+for (let i = 0; i < panaderia.length; i++) {
+    mensaje += `${panaderia[i].nombre} (${panaderia[i].localidad}provincia)`;
+}
+
+    alert(mensaje);
+    consultas();
+
+
+// carga de usuario
+
+function nuevoCliente () {
+
+    let panaderia = [];
+    let user = true;
+
+    while (user) {
+        let DNI = Number (prompt("Ingrese su DNI"));
+        let NOMBRE = prompt("Ingrese su Nombre Completo");
+        let DOMICILIO = prompt ("Ingrese su Domicilio ");
+
+        return panaderia
+        
+    }
+    
+}
+
 
 constructor(pan de campo, medialunas, sanguches, precio, imagen){
 
@@ -15,15 +46,15 @@ this.vendido = false;
 let productos = prompt("Ingrese que producto desea");
 
 
-const productos1 = new productos (pan de campo: "pan de campo", precio : 300, imagen: imagen);
+const productos1 = new productos (pan de campo: "pan de campo",precio: 300,imagen: imagen);
 console.log(prompt("productos1"));
 alert ("El producto ingresado es " + productos1);
 
-const productos2 = new productos (medialunas: "medialunas", precio : 600 , imagen: imagen);
+const productos2 = new productos (medialunas: "medialunas", precio: 600 , imagen: imagen);
 console.log(prompt("productos2"));
 alert ("El producto ingresado es " + productos2);
 
-const productos3 = new productos (sanguches: "sanguches", precio: 250, imagen: "imagen");
+const productos3 = new productos (sanguches: "sanguches",precio: 250,imagen: "imagen");
 console.log(prompt("productos3"));
 alert ("El producto ingresado es " + productos3);
 }
