fa53377f7ce4f0412ff7d4a8d49fc54a98c56973
facusteckler86 <facundomsteckler@gail.com>
2023-03-20 01:46:32 -0300

    agrego css

--- index.js
+++ index.js
@@ -2,36 +2,11 @@
 
 //saludo inicial.
 
-panaderia = cargaClientes ();
+let usuario = prompt("Hola, Bienvenido, indique su usuario");
 
-let mensaje = "Bienvenid@: ";
-
-for (let i = 0; i < panaderia.length; i++) {
-    mensaje += `${panaderia[i].nombre} (${panaderia[i].localidad}provincia)`;
-}
-
-    alert(mensaje);
-    consultas();
-
-
-// carga de usuario
-
-function nuevoCliente () {
-
-    let panaderia = [];
-    let user = true;
-
-    while (user) {
-        let DNI = Number (prompt("Ingrese su DNI"));
-        let NOMBRE = prompt("Ingrese su Nombre Completo");
-        let DOMICILIO = prompt ("Ingrese su Domicilio ");
-
-        return panaderia
-        
+while (usuario != "Facundo") {
+    alert("El usuario es incorrecto");
     }
-    
-}
-
 //constructor de productos.
 
 const productos=[
@@ -45,22 +20,4 @@ const productos=[
 
 //consulto que producto de los 3 en tienda on-line quiere, mediante un prompt
 
-let productos = prompt("Ingrese que producto desea");
-
-//pan de campo
-
-const productos1 = new productos (pan de campo: "pan de campo",precio: 300,imagen: imagen);
-console.log(prompt("productos1"));
-alert ("El producto ingresado es " + productos1);
-
-// medialunas
-
-const productos2 = new productos (medialunas: "medialunas", precio: 600 , imagen: imagen);
-console.log(prompt("productos2"));
-alert ("El producto ingresado es " + productos2);
-
-//sanguches
-
-const productos3 = new productos (sanguches: "sanguches",precio: 250,imagen: "imagen");
-console.log(prompt("productos3"));
-alert ("El producto ingresado es " + productos3);
+let consulta = prompt("Ingrese que producto desea");
