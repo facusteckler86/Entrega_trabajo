f857d93745586f8aa62f4c6a2763be640697f497
facusteckler86 <facundomsteckler@gail.com>
2023-03-20 02:06:21 -0300

    agrego JSON

--- index.js
+++ index.js
@@ -4,9 +4,39 @@
 
 let usuario = prompt("Hola, Bienvenido, indique su usuario");
 
-while (usuario != "Facundo") {
-    alert("El usuario es incorrecto");
-    }
+let formulario = document.getElementById("Formulario");
+e.preventDefault();
+
+formulario.addEventListener("submit", (e) => {
+
+        let inputs = e.target.children;
+        let nombre = inputs[0].value;
+        let domicilio = inputs[1].value;
+        let telefono = inputs[2];
+
+        
+        let objeto = {
+            nombre,
+            domicilio,
+            telefono,
+        };
+
+        
+        let usuarioStorage = localStorage.getItem("usuario");
+        let usuario;
+
+        if (usuario) {
+            usuario = JSON.parse(usuarioStorage);
+            
+        } else{
+            usuario [];
+        }
+
+        usuario.push(objeto);
+        localStorage.setItem("usuario", JSON.stringify(usuario));
+        
+    });    
+    
 //constructor de productos.
 
 const productos=[
