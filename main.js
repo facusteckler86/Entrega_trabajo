7b326f67ae60964d80b7f454bcaecc7519df2282
facusteckler86 <facundomsteckler@gmail.com>
2023-04-13 00:29:35 -0300

    images

---
+++ main.js
@@ -0,0 +1,62 @@
+// modelo de carrito de compra para panaderia
+
+let usuario = prompt("Hola, Bienvenido, indique su usuario");
+
+formulario.addEventListener("submit", (e) => {
+        let formulario = document.getElementById("Formulario");
+        e.preventDefault();
+
+        let inputs = e.target.children;
+        let nombre = inputs[0].value;
+        let domicilio = inputs[1].value;
+        let telefono = inputs[2];
+        
+        let objeto = {
+            nombre,
+            domicilio,
+            telefono,
+        };
+        
+        let usuarioStorage = localStorage.getItem("usuario");
+        let usuarioGuardado;
+
+        if (usuarioStorage) {
+            usuario = JSON.parse(usuarioStorage);
+            
+        } else{
+            usuarioGuardado = [];
+        }
+
+        usuarioGuardado.push(objeto);
+        localStorage.setItem("usuario", JSON.stringify(usuarioGuardado));
+        
+        //constructor de productos.
+
+const productos=[
+    
+  {id: 1, nombre: "pan de campo",precio: 750},
+  {id: 2, nombre: "medialunas",precio: 1500},
+  {id: 3, nombre: "sanguches",precio: 350},
+  {id: 4, nombre: "rosquitas",precio: 500},
+      
+  ];
+
+  let boton = document.getElementById("boton");
+  
+  boton.addEventListener("click", () => {
+      Swal.fire({
+        title: "Agregue el producto",
+        input: "text",
+        showCancelButton: true,
+        confirmButtonText: "Look up",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: result.value,
+          });
+        }
+      });
+    });
+
+
+    }); 
